3441bb958333dfa05e226424f082b87e0019fe87
Robert Samuel <robert_samuel97@hotmail.com>
2024-04-15 16:19:45 -0600

    Notes - Start to create the endpoint that will create notes

--- app/api/notes/route.ts
+++ app/api/notes/route.ts
@@ -24,7 +24,37 @@ export async function GET(req: NextApiRequest, res: NextApiResponse) {
   } catch (error) {
     console.log('error from try catch', error)
     return NextResponse.json(
-      { message: 'Error fetching notes' },
+      { message: 'Error fetching note.s' },
+      { status: 400 }
+    );
+  }
+};
+
+export async function POST(req: NextApiRequest, res: NextApiResponse) {
+  try {
+    const supabase = createClient();
+    const { data: { user } } = await supabase.auth.getUser(); 
+
+    const { data, error } = await supabase
+      .from('notes')
+      .insert({
+        text: 'the text',
+        createdBy: user?.id
+      });
+
+    if(error) {
+      console.log('error retrieving notes', error)
+      return NextResponse.json(
+        { message: 'Error fetching notes' },
+        { status: 400 }
+      );
+    }
+
+    return NextResponse.json({data}, { status: 200 })
+  } catch (error) {
+    console.log('error from try catch', error)
+    return NextResponse.json(
+      { message: 'Error trying to save the note.' },
       { status: 400 }
     );
   }
